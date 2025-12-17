736903331abab734139b9498336b68bd76ee6385
bala-amypo <bala@amypo.in>
2025-12-17 06:25:57 +0000

    User commit

--- src/main/java/com/example/demo/model/EmployeeProfile.java
+++ src/main/java/com/example/demo/model/EmployeeProfile.java
@@ -1,3 +1,18 @@
+import jakarata.presistence.*;
+import java.time.LocalDataTime;
+
+@Entity
+@Table(
+    name = "employee_profile",
+    uniqueConstraints = {
+        @uniqueConstraints(coulumnNames = "employeeId")
+        @uniqueConstraints(coulumnNames = "email")
+    }
+)
+
 public class EmployeeProfile{
+    @Id 
+    @GeneratedValue(strategy = GenrationType.IDENTITY)
+    private Long id
     
 }
