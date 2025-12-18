13cba2672add807c0dce2b2f18aa6fe171a6df43
bala-amypo <bala@amypo.in>
2025-12-18 04:07:47 +0000

    User commit

--- src/main/java/com/example/demo/model/ProductivityMetricRecord.java
+++ src/main/java/com/example/demo/model/ProductivityMetricRecord.java
@@ -20,3 +20,90 @@ public ProductivityMetricRecord{
     private Double hoursLogged;
 
     private int tasksCompleted;
+
+    private int meetingAttended;
+
+    private Double productivityScore;
+
+    private String rawDataJson;
+
+    private LocalDateTime submittedAt;
+
+     @PrePersist
+    public void prePersist(){
+        this.createdAt = LocalDateTime.now();
+    }
+    
+
+  public Long getId() {
+    return id;
+}
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public Long getEmployeeId() {
+    return employeeId;
+}
+
+public void setEmployeeId(Long employeeId) {
+    this.employeeId = employeeId;
+}
+
+public LocalDate getDate() {
+    return date;
+}
+
+public void setDate(LocalDate date) {
+    this.date = date;
+}
+
+public Double getHoursLogged() {
+    return hoursLogged;
+}
+
+public void setHoursLogged(Double hoursLogged) {
+    this.hoursLogged = hoursLogged;
+}
+
+public Integer getTasksCompleted() {
+    return tasksCompleted;
+}
+
+public void setTasksCompleted(Integer tasksCompleted) {
+    this.tasksCompleted = tasksCompleted;
+}
+
+public Integer getMeetingsAttended() {
+    return meetingsAttended;
+}
+
+public void setMeetingsAttended(Integer meetingsAttended) {
+    this.meetingsAttended = meetingsAttended;
+}
+
+public Double getProductivityScore() {
+    return productivityScore;
+}
+
+public void setProductivityScore(Double productivityScore) {
+    this.productivityScore = productivityScore;
+}
+
+public String getRawDataJson() {
+    return rawDataJson;
+}
+
+public void setRawDataJson(String rawDataJson) {
+    this.rawDataJson = rawDataJson;
+}
+
+public LocalDateTime getSubmittedAt() {
+    return submittedAt;
+}
+
+public void setSubmittedAt(LocalDateTime submittedAt) {
+    this.submittedAt = submittedAt;
+}
+
