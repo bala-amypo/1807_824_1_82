3e82059a94cbac617edd59148edb9c0614fbbfcf
bala-amypo <bala@amypo.in>
2025-12-18 10:13:27 +0000

    User commit

--- src/main/java/com/example/demo/services/implement/ProductivityMetricImplementation.java
+++ src/main/java/com/example/demo/services/implement/ProductivityMetricImplementation.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.example.demo.service.ProductivityMetricService;
 import com.example.demo.model.ProductivityMetricRecord;
 import com.example.demo.repository.ProductivityMetricRepository;
 
