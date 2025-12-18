217587110d0bd7049f5230890456e1f5fac762a4
bala-amypo <bala@amypo.in>
2025-12-18 04:02:42 +0000

    User commit

--- src/main/java/com/example/demo/model/ProductivityMetricRecord.java
+++ src/main/java/com/example/demo/model/ProductivityMetricRecord.java
@@ -1,3 +1,22 @@
-// public class ProductivityMetricRecord {
+package com.example.demo.model;
+import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+@Entity
+public ProductivityMetricRecord{
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     
-// }
+    private String employeeId;
+
+    private LocalDate date;
+
+    private Double hoursLogged;
+
+    private int tasksCompleted;
