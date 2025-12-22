15041656afcd87bc0b5cb833a77264758b1bc07b
bala-amypo <bala@amypo.in>
2025-12-22 09:28:46 +0000

    User commit

--- src/main/java/com/example/demo/services/implement/AnomalyFlagImplementation.java
+++ src/main/java/com/example/demo/services/implement/AnomalyFlagImplementation.java
@@ -1,3 +1,12 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.AnomalyFlag;
+import com.example.demo.repository.AnomalyFlagRepository;
 @Service
 public class AnomalyFlagImplementation implements AnomalyFlagService {
 
