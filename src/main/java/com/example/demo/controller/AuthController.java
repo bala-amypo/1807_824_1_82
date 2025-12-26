884f616b601c42cd737b6e43d2fc30353c4a10e8
bala-amypo <bala@amypo.in>
2025-12-26 12:52:51 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.security.JwtUtil;
+import com.example.demo.security.JwtTokenProvider;
 import com.example.demo.dto.LoginRequest;
 import com.example.demo.dto.LoginResponse;
 
