6b664f813333e99493c157b248638f824e78051c
bala-amypo <bala@amypo.in>
2025-12-26 16:54:15 +0000

    User commit

--- src/main/java/com/example/demo/repository/EmployeeProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import java.util.List;
-import java.util.Optional;
-import com.example.demo.model.*;
-
-public interface EmployeeProfileRepository {
-    Optional<EmployeeProfile> findByEmployeeId(String employeeId);
-}
--- src/main/java/com/example/demo/repository/ProductivityMetricRecordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import java.util.List;
-import com.example.demo.model.ProductivityMetricRecord;
-
-public interface ProductivityMetricRecordRepository {
-    List<ProductivityMetricRecord> findByEmployeeId(Long employeeId);
-}
