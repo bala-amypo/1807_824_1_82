877f615953345c0ddb6795cea35927fba3c5ccb3
bala-amypo <bala@amypo.in>
2025-12-28 14:49:46 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,8 +1,6 @@
 package com.example.demo.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.security.core.Authentication;
+import io.jsonwebtoken.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -10,26 +8,25 @@ import java.util.Date;
 @Component
 public class JwtTokenProvider {
 
-    private final String jwtSecret = "secret123";
-    private final long jwtExpirationMs = 86400000; // 1 day
+    private final String SECRET_KEY = "my-secret-key";
+    private final long EXPIRATION_TIME = 60 * 60 * 1000; // 1 hour
 
-    public String generateToken(Authentication authentication) {
-
-        String username = authentication.getName();
+    public String generateToken(String username) {
 
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(
-                        new Date(System.currentTimeMillis() + jwtExpirationMs)
+                        new Date(System.currentTimeMillis() + EXPIRATION_TIME)
                 )
-                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
 
     public String getUsernameFromToken(String token) {
+
         return Jwts.parser()
-                .setSigningKey(jwtSecret)
+                .setSigningKey(SECRET_KEY)
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
@@ -37,9 +34,9 @@ public class JwtTokenProvider {
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
             return true;
-        } catch (Exception e) {
+        } catch (JwtException | IllegalArgumentException e) {
             return false;
         }
     }
