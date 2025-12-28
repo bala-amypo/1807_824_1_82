309dde97ee9075bc839cbc3c5990f8c5730727f3
bala-amypo <bala@amypo.in>
2025-12-28 02:54:27 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,10 +1,46 @@
+// package com.example.demo.config;
+
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.servers.Server;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import java.util.List;
+
+// @Configuration
+// public class SwaggerConfig {
+
+//     @Bean
+//     public OpenAPI customOpenAPI() {
+//         return new OpenAPI()
+//                 // You need to change the port as per your server
+//                 .servers(List.of(
+//                         new Server().url("https://9010.32procr.amypo.ai/")
+//                 ));
+//         }
+        
+//     @Bean
+//     public OpenAPI customOpenAPI() {
+
+//         SecurityScheme bearerAuth = new SecurityScheme()
+//                 .type(SecurityScheme.Type.HTTP)
+//                 .scheme("bearer")
+//                 .bearerFormat("JWT");
+
+//         return new OpenAPI()
+//                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+//                 .components(
+//                         new Components().addSecuritySchemes("bearerAuth", bearerAuth)
+//                 );
+//     }
+
+// }
+        
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,27 +48,10 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9010.32procr.amypo.ai/")
-                ));
-        }
-        
-    @Bean
-    public OpenAPI customOpenAPI() {
-
-        SecurityScheme bearerAuth = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
-        return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(
-                        new Components().addSecuritySchemes("bearerAuth", bearerAuth)
-                );
+                .info(new Info()
+                        .title("Employee Productivity Monitoring API")
+                        .version("1.0")
+                        .description("Backend Frameworks Project"));
     }
-
 }
-        
 
